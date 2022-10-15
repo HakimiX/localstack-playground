287d386a6e9f58e5e64e4092971337c964ca526d
Mustafa Hakimi <mustafa.hakimi@outlook.dk>
2022-10-15 13:42:01 +0200

    display injected env var

--- lambda-container/src/handler.py
+++ lambda-container/src/handler.py
@@ -1,10 +1,14 @@
+import os
 import logging
+
+SOME_BUCKET = os.getenv('SOME_BUCKET')
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def handler(event, context):
     logger.info('incoming event: {}'.format(event))
+    logger.info('Environment variable: {}'.format(SOME_BUCKET))
 
     return {
       'statusCode': 200,
